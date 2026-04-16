5041f1c88c1c08d7cca1f204945102e069c25dcd
Bhargav Alimili <bhargavalimili@gmail.com>
2026-04-16 00:32:12 -0500

    Pharma db changes

--- snowflake/pharma_db.sql
+++ snowflake/pharma_db.sql
@@ -31,3 +31,85 @@ CREATE TABLE weekday_sales (
 );
 
 SHOW TABLES IN SCHEMA pharma_db.gold;
+
+-- Create S3 integration
+CREATE STORAGE INTEGRATION s3_pharma_integration
+    TYPE = EXTERNAL_STAGE
+    STORAGE_PROVIDER = 'S3'
+    ENABLED = TRUE
+    STORAGE_ALLOWED_LOCATIONS = ('s3://pharma-sales-pipeline/gold/')
+    STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::045230654036:role/GluePharmaRole';
+
+    DESC INTEGRATION s3_pharma_integration;
+
+    -- File format for Parquet
+CREATE FILE FORMAT parquet_format
+    TYPE = PARQUET
+    SNAPPY_COMPRESSION = TRUE;
+
+-- Stage for monthly drug sales
+CREATE STAGE stage_monthly_drug_sales
+    URL = 's3://pharma-sales-pipeline/gold/monthly_drug_sales/'
+    STORAGE_INTEGRATION = s3_pharma_integration
+    FILE_FORMAT = parquet_format;
+
+-- Stage for top drugs
+CREATE STAGE stage_top_drugs
+    URL = 's3://pharma-sales-pipeline/gold/top_drugs/'
+    STORAGE_INTEGRATION = s3_pharma_integration
+    FILE_FORMAT = parquet_format;
+
+-- Stage for weekday sales
+CREATE STAGE stage_weekday_sales
+    URL = 's3://pharma-sales-pipeline/gold/weekday_sales/'
+    STORAGE_INTEGRATION = s3_pharma_integration
+    FILE_FORMAT = parquet_format;
+
+LIST @stage_monthly_drug_sales;
+LIST @stage_top_drugs;
+LIST @stage_weekday_sales;
+
+COPY INTO monthly_drug_sales
+FROM (
+    SELECT
+        $1:Year::INT,
+        $1:Month::INT,
+        $1:drug_name::VARCHAR,
+        $1:total_units_sold::FLOAT,
+        $1:avg_daily_units::FLOAT,
+        $1:trading_days::INT
+    FROM @stage_monthly_drug_sales
+)
+FILE_FORMAT = (TYPE = PARQUET)
+ON_ERROR = CONTINUE;
+
+SELECT COUNT(*) FROM monthly_drug_sales;
+SELECT * FROM monthly_drug_sales LIMIT 5;
+
+COPY INTO top_drugs
+FROM (
+    SELECT
+        $1:drug_name::VARCHAR,
+        $1:total_units_sold::FLOAT,
+        $1:avg_daily_units::FLOAT
+    FROM @stage_top_drugs
+)
+FILE_FORMAT = (TYPE = PARQUET)
+ON_ERROR = CONTINUE;
+
+SELECT COUNT(*) FROM top_drugs;
+SELECT * FROM top_drugs LIMIT 5;
+
+COPY INTO weekday_sales
+FROM (
+    SELECT
+        $1:weekday_name::VARCHAR,
+        $1:drug_name::VARCHAR,
+        $1:avg_units_sold::FLOAT
+    FROM @stage_weekday_sales
+)
+FILE_FORMAT = (TYPE = PARQUET)
+ON_ERROR = CONTINUE;
+
+SELECT COUNT(*) FROM weekday_sales;
+SELECT * FROM weekday_sales LIMIT 5;
